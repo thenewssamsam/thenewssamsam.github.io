15cf1510e4b402d2e84871378d0c97527b096298
Sam <yenyaosam@gmail.com>
2026-08-26 08:57:04 +0800

    Replace PWA service worker with ad service worker
    - Replace original PWA sw.js with ad service worker
    - Use monetag ad service worker for monetization
    - Domain: 5gvci.com, ZoneId: 11654361

--- sw.js
+++ sw.js
@@ -1,51 +1,6 @@
-// Service Worker for Offline Support and Caching
-// GitHub Pages PWA Support
-
-const CACHE_NAME = 'thenewssam-v1';
-const urlsToCache = [
-  '/',
-  '/about.html',
-  '/sitemap.xml'
-];
-
-// Install event - cache assets
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Fetch event - serve from cache, fallback to network
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-// Activate event - clean up old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11654361
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
